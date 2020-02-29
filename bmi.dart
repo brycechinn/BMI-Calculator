0b2e949ffd6d4479a7567e07342fda7f1e0a7fb4
brycechinn <33844159+brycechinn@users.noreply.github.com>
2020-02-28 16:30:50 -0800

    First commit

---
+++ bmi.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'dart:math';
+
+void main()
+{
+    //double height;
+    //double weight;
+
+    print("Welcome to the BMI calculator!");
+
+    print("Please enter your height in inches: ");
+    double height = double.parse(stdin.readLineSync());
+
+    print("Please enter your weight in pounds: ");
+    double weight = double.parse(stdin.readLineSync());
+    
+    double bmi = (703 * weight) / pow(height, 2); //calculates bmi
+
+    bmi = double.parse((bmi).toStringAsFixed(2)); //rounds bmi to two decimal places
+    
+    print("Your BMI is $bmi.");
+}
